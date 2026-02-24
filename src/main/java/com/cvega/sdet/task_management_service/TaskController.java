1a2f14e615f9425e4af76c7d74a60e2a055ac16a
cvega <carlosjuniorrvega@hotmail.com>
2026-02-23 20:14:18 -0500

    feat: swagger ui fully functional and returning task list

--- src/main/java/com/cvega/sdet/task_management_service/TaskController.java
+++ src/main/java/com/cvega/sdet/task_management_service/TaskController.java
@@ -1,17 +1,14 @@
-    package com.cvega.sdet.task_management_service;
+package com.cvega.sdet.task_management_service;
 
-    import org.springframework.web.bind.annotation.GetMapping;
-    import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-    import java.util.List;
+import java.util.List;
 
-    @RestController
-    public class TaskController {
-        @GetMapping(path = "/docs")
-        public void redirect(jakarta.servlet.http.HttpServletResponse response) throws java.io.IOException {
-            response.sendRedirect("/swagger-ui/index.html");
-        }
-        public List<String> listGenerator() {
-            return List.of("wake up", "get ready", "coding");
-        }
+@RestController
+public class TaskController {
+    @GetMapping(path = "/")
+    public List<String> listGenerator() {
+        return List.of("wake up", "get ready", "coding");
     }
+}
