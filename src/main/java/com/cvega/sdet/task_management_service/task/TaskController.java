5bf1f70334a295927ed6561f716c3a1006458308
cvega <carlosjuniorrvega@hotmail.com>
2026-03-04 13:48:13 -0500

    feat(task): inject TaskService class via constructor and implement getTask. CV

---
+++ src/main/java/com/cvega/sdet/task_management_service/task/TaskController.java
@@ -0,0 +1,25 @@
+package com.cvega.sdet.task_management_service.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/tasks")
+public class TaskController {
+    private final TaskService taskService;
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    @GetMapping
+    public List<Task> getTasks(){
+        return  taskService.listGenerator();
+    }
+}
+
+
+
