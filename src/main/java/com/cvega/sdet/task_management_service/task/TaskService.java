a515072929bde780796ca9188b23a2bb614ad623
cvega <carlosjuniorrvega@hotmail.com>
2026-03-15 16:40:46 -0500

    feat(task): inject taskRepository for access to data from database.

--- src/main/java/com/cvega/sdet/task_management_service/task/TaskService.java
+++ src/main/java/com/cvega/sdet/task_management_service/task/TaskService.java
@@ -1,5 +1,6 @@
 package com.cvega.sdet.task_management_service.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,8 +9,14 @@ import java.time.Month;
 import java.util.List;
 @Service
 public class TaskService {
+    private final TaskRepository taskRepository;
 
-    public List<Task> listGenerator() {
-        return List.of(new Task(111111L, "Build a framework with selenium", LocalDate.of(2026, Month.MARCH, 2), 1), new Task(22222L, "make framework more efficient", LocalDate.of(2026, Month.MARCH, 9), 2));
+    @Autowired
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
     }
+    public List<Task> listGenerator() {
+        return this.taskRepository.findAll(); /*List.of(new Task(111111L, "Build a framework with selenium", LocalDate.of(2026, Month.MARCH, 2), 1), new Task(22222L, "make framework more efficient", LocalDate.of(2026, Month.MARCH, 9), 2));*/
+    }
+
 }
