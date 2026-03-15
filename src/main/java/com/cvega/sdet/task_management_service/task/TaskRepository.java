cbb7f5977868b552b7fe3ecfc341607496a1e803
cvega <carlosjuniorrvega@hotmail.com>
2026-03-15 16:37:18 -0500

    feat(task): create interface TaskRepository for data access.

---
+++ src/main/java/com/cvega/sdet/task_management_service/task/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.cvega.sdet.task_management_service.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
