8f51a416f3cf7adc9e53bdae57e3d1ddf5ae3e34
cvega <carlosjuniorrvega@hotmail.com>
2026-03-19 11:04:26 -0500

    feat(task): implement task registration via Post endpoint in controller

--- src/main/java/com/cvega/sdet/task_management_service/task/TaskController.java
+++ src/main/java/com/cvega/sdet/task_management_service/task/TaskController.java
@@ -1,9 +1,7 @@
 package com.cvega.sdet.task_management_service.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,13 +9,20 @@ import java.util.List;
 @RequestMapping(path = "/api/v1/tasks")
 public class TaskController {
     private final TaskService taskService;
+
     @Autowired
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
+
     @GetMapping
-    public List<Task> getTasks(){
-        return  taskService.listGenerator();
+    public List<Task> getTasks() {
+        return taskService.listGenerator();
+    }
+
+    @PostMapping
+    public void registerTask(@RequestBody Task task) {
+        this.taskService.createNewTask(task);
     }
 }
 
