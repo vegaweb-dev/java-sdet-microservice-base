dd99bed88f6fce55f70e1175b6f9727792031c2d
cvega <carlosjuniorrvega@hotmail.com>
2026-03-19 11:10:18 -0500

    feat(repo): add findByTaskDescription query method

--- src/main/java/com/cvega/sdet/task_management_service/task/TaskRepository.java
+++ src/main/java/com/cvega/sdet/task_management_service/task/TaskRepository.java
@@ -3,7 +3,9 @@ package com.cvega.sdet.task_management_service.task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+    Optional<Task> findByTaskDescription(String taskDescription);
 }
