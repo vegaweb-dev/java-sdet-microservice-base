594d2505705f149120c7c24b28738124bc54ce6f
cvega <carlosjuniorrvega@hotmail.com>
2026-03-19 11:15:51 -0500

    feat(service): add task creation with duplicate check

--- src/main/java/com/cvega/sdet/task_management_service/task/TaskService.java
+++ src/main/java/com/cvega/sdet/task_management_service/task/TaskService.java
@@ -7,16 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository){
+    public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
+
     public List<Task> listGenerator() {
         return this.taskRepository.findAll(); /*List.of(new Task(111111L, "Build a framework with selenium", LocalDate.of(2026, Month.MARCH, 2), 1), new Task(22222L, "make framework more efficient", LocalDate.of(2026, Month.MARCH, 9), 2));*/
     }
 
+    public void createNewTask(Task task) {
+        Optional<Task> res = taskRepository.findByTaskDescription(task.getTaskDescription());
+        if (res.isPresent()) {
+            throw new IllegalArgumentException("This task is already registered");
+        }
+        taskRepository.save(task);
+    }
 }
