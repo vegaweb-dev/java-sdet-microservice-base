25ff95c32cd12710c84c3da441f5d75a657eee84
cvega <carlosjuniorrvega@hotmail.com>
2026-03-27 15:13:04 -0500

    test(task-service): add unit test for posting tasks

--- src/test/java/com/cvega/sdet/task_management_service/unit/task/TaskServiceTest.java
+++ src/test/java/com/cvega/sdet/task_management_service/unit/task/TaskServiceTest.java
@@ -6,8 +6,10 @@ import com.cvega.sdet.task_management_service.task.TaskService;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class TaskServiceTest {
@@ -24,24 +26,33 @@ public class TaskServiceTest {
         //Assert
         assertEquals(fakeList, tasks);
         verify(repositoryMock).findAll();
-
-
     }
 
     @Test
-    public void shouldCreateNewTask() {
+    public void shouldSaveNewTaskWhenDescriptionDoesNotExist() {
         //Arrange
+        TaskRepository repositoryMock = mock(TaskRepository.class);
+        TaskService taskService = new TaskService(repositoryMock);
+        Task task = new Task();
+        task.setTaskDescription("this is the description of new task to be saved");
+        when(repositoryMock.findByTaskDescription(task.getTaskDescription())).thenReturn(Optional.empty());
         //Act
+        taskService.createNewTask(task);
         //Assert
-
+        verify(repositoryMock, times(1)).save(task);
     }
 
     @Test
     public void shouldInformTaskAlreadyExists() {
         //Arrange
-        //Act
-        //Assert
-
+        TaskRepository repositoryMock = mock(TaskRepository.class);
+        TaskService taskService = new TaskService(repositoryMock);
+        Task task = new Task();
+        task.setTaskDescription("this is the new existing task");
+        when(repositoryMock.findByTaskDescription(task.getTaskDescription())).thenReturn(Optional.of(task));
+        //Act + Assert
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> taskService.createNewTask(task));
+        assertEquals("This task is already registered", exception.getMessage());
+        verify(repositoryMock, never()).save(task);
     }
-
 }
